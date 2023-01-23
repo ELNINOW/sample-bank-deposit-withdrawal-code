56a0b65ca4b3ed5cb29ff3b593fafe896e1ba013
Kalana Madusara <116989714+ELNINOW@users.noreply.github.com>
2023-01-23 14:46:59 +0530

    Add files via upload

---
+++ Member methods.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+class Account{
+    
+private:
+
+    string name;
+    double balance;
+    
+public:
+
+    void set_balance(double bal){
+    balance= bal;}
+    
+    double get_balance(){
+    return balance;}
+    
+    void set_name(string n);
+    string get_name();
+    
+    bool deposit(double amount);
+    bool withdraw(double amount);
+    
+    };
+    
+void Account::set_name(string n){
+    name=n;
+} 
+
+string Account::get_name(){
+    return name;
+    
+}
+
+bool Account::withdraw(double amount){
+    if (balance-amount>=0){
+        balance-=amount;
+        return true;
+    }
+    else{
+        return false; 
+        
+    }
+    }
+
+
+int main(){
+    Account frank_account;
+    frank_account.set_name("Franks account");
+    frank_account.set_balance(1000.0);
+    
+    if (frank_account.deposit(200.0))
+        cout<<"Deposit ok"<<endl;
+        
+    elsecout<<"Deposit not allowed"<<endl;
+    
+    if(frank_account.withdraw(500.0))
+        cout<<"Withdrawal ok" << endl;
+    else
+        cout<<"Not sufficient funds"<< endl;
+    if(frank_account.withdraw(1500.0))
+        cout<<"Withdraw ok"<<endl;
+    else
+        cout<< "No sufficient funds"<<endl;
+        
+    if (frank account.withdraw(1500.0))
+        cout<<"withdraw okay"<<endl;
+        
+    
+        
+}
+
+    
+    
+    
